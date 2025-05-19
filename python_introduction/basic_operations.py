6eafb29d9b990807c6db6f00dcb94f769a7ee2bf
mike <mikeukachi@outlook.com>
2025-05-20 00:40:34 +0100

    alx python intro task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+# Addition
+result1 = number1 + number2
+#subtraction
+result2 = number1 - number2
+# Multiplication
+result3= number1 * number2
+print("Addition of " , number1 , " and " , number2 , " is " , result1)
+print("Subtraction of " , number1 , " and " , number2 , " is " , result2)
+print("Multiplication of " , number1 , " and " , number2 , " is " , result3)
