1e706966bfec1c6bea38c928278ac0f37def9d39
Mike-ukachi <mikeukachi@outlook.com>
2025-06-07 11:16:35 +0100

    completed alx task 1 and 2

---
+++ fna_and_dsa/arithemetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operations(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                return "Error: Division by zero"
+        case _:
+            return "Error: Unknown operation"
